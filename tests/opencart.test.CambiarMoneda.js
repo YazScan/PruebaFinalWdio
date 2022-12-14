87e2aeeebca0b371f5137e1a200376ee460df874
Yaz <Yazscanziani@gmail.com>
2022-12-14 19:19:37 -0300

    Arregle unas diferencias

--- tests/opencart.test.CambiarMoneda.js
+++ tests/opencart.test.CambiarMoneda.js
@@ -1,17 +1,17 @@
-// import homePage from '../pages/home.page';
-// import busquedaPage from '../pages/busqueda.page';
-// import DATOS from '../datos/articulos';
+import homePage from '../pages/home.page';
+import busquedaPage from '../pages/busqueda.page';
+import DATOS from '../datos/articulos';
  
-// describe('Cambiar la moneda', function () {
+describe('Cambiar la moneda', function () {
 
-//   it('Deberia cambiar la moneda a Euro', async ()=>{
-//     await homePage.abrir('/');
-//     await homePage.currency.click();
-//     await homePage.currencyDropDown.click();
-//     await assert.equal(await $('strong').getText(),'€','Error: la moneda no es la que deberia');
-//     await browser.pause(5000);
+  it('Deberia cambiar la moneda a Euro', async ()=>{
+    await homePage.abrir('/');
+    await homePage.currency.click();
+    await homePage.currencyDropDown.click();
+    await assert.equal(await $('strong').getText(),'€','Error: la moneda no es la que deberia');
+    await browser.pause(5000);
 
-//   });
+  });
 
-// });
+});
 
