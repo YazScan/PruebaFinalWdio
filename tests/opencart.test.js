d7043029b71119d89499b75133ea7f8a22af3124
Yaz <Yazscanziani@gmail.com>
2022-12-14 19:18:29 -0300

    Auto stash before merge of "main" and "origin/main"

---
+++ tests/opencart.test.js
@@ -0,0 +1,16 @@
+import homePage from '../pages/home.page';
+import busquedaPage from '../pages/busqueda.page';
+import DATOS from '../datos/articulos';
+ 
+describe('Cambiar la moneda', function () {
+
+  it('Deberia cambiar la moneda a Euro', async ()=>{
+    await homePage.abrir('/');
+    await homePage.currency.click();
+    await homePage.currencyDropDown.click();
+    //let information= await $('=site')
+    //await information.scrollIntoView();
+    await browser.pause(5000);
+  });
+   
+});
