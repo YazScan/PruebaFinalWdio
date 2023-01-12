41c941b6e3bc4943178e6dd63cf250eb906ebc89
YazScan <Yazscanziani@gmail.com>
2023-01-12 16:59:29 -0300

    uncommented test

--- tests/changeCurrency.Test.js
+++ tests/changeCurrency.Test.js
@@ -1,27 +1,27 @@
-// import homePage from '../pages/home.page';
-// import {assert} from 'chai';
+import homePage from '../pages/home.page';
+import {assert} from 'chai';
 
-// let currency= '£';
+let currency= '£';
 
-// describe('Change currency',  () => {
-//   before('Open main page', async () => {
-//     await homePage.open('/');
-//     assert.equal(await homePage.homePageTitle.getText(),'Featured','Error:the page did not initialize well');
-// });
-//   it('[CP-Currency-01] Should change currency to the one selected', async ()=>{
-//     addStep('Selects currency dropdown');
-//     await homePage.currency.click();
-//     addStep('Selects the currency choosen');
-//     await homePage.currencySelector(currency);
-//     addStep('Checks that the currency has changed');
-//     assert.equal(await homePage.currencyElement.getText(),currency,'Error: The currency is not what it should');
-//     addStep('Selects currency dropdown');
-//     await homePage.currency.click();
-//     addStep('leaves the web page in its initial currency');
-//     currency = '$';
-//     await homePage.currencySelector(currency) ;
-//     addStep('Checks the final currency');
-//     assert.equal(await homePage.currencyElement.getText(),currency,'Error: The test did not end on the correct currency');
-// });
-// });
+describe('Change currency',  () => {
+    before('Open main page', async () => {
+        await homePage.open('/');
+        assert.equal(await homePage.homePageTtl.getText(),'Featured','Error:the page did not initialize well');
+    });
+        it('[CP-Currency-01] Should change currency to the one selected', async ()=>{
+            addStep('Selects currency dropdown');
+            await homePage.currency.click();
+            addStep('Selects the currency choosen');
+            await homePage.currencySelector(currency);
+            addStep('Checks that the currency has changed');
+            assert.equal(await homePage.currencyElement.getText(),currency,'Error: The currency is not what it should');
+            addStep('Selects currency dropdown');
+            await homePage.currency.click();
+            addStep('leaves the web page in its initial currency');
+            currency = '$';
+            await homePage.currencySelector(currency) ;
+            addStep('Checks the final currency');
+            assert.equal(await homePage.currencyElement.getText(),currency,'Error: The test did not end on the correct currency');
+        });
+});
 
--- tests/checkShoppginCartAfterSearch.Test.js
+++ tests/checkShoppginCartAfterSearch.Test.js
@@ -1,30 +1,29 @@
-// import homePage from '../pages/home.page';
-// import DATOS from '../data/articles';
-// import {assert} from 'chai';
-// import searchPage from '../pages/search.page';
+import homePage from '../pages/home.page';
+import DATOS from '../data/articles';
+import {assert} from 'chai';
+import searchPage from '../pages/search.page';
 
-// describe('Search articles and check that the shopping cart did not change',  () => {
-//     before('Open main page', async () => {
-//         await homePage.open('/');
-//         assert.equal(await homePage.homePageTitle.getText(),'Featured','Error:the page did not initialize well');
-//     });
+describe('Search articles and check that the shopping cart did not change',  () => {
+    before('Open main page', async () => {
+        await homePage.open('/');
+        assert.equal(await homePage.homePageTtl.getText(),'Featured','Error:the page did not initialize well');
+    });
 
-//     DATOS.forEach(({article}) => {
+    DATOS.forEach(({article}) => {
      
-//         it(`Should search ${article}`, async ()=> {
+        it(`[CP-ShoppinCartCheck-01] Should search ${article} and check the shopping cart`, async ()=> {
             
-//             addStep('Search articles')
-//             await homePage.search(article);
-//             await expect(await homePage.getSearchText()).to.equal(article);
-//             await expect(await searchPage.resultName()).to.equal(article);
-//             addStep('Checks that the shopping cart did not change')
-//             await $(".btn-inverse").waitForDisplayed();
-//             addStep('compare images')
-//             expect(
-//                 await browser.checkElement(await $(".btn-inverse"), "opencart-ShoppingCart", {
-//                 }),
-//                 "Error: Shopping Cart has changed"
-//             ).equal(0);
-//         });
-//     });
-// });
+            addStep('Search articles')
+            await homePage.search(article);
+            await expect(await homePage.getSearchText()).to.equal(article);
+            await expect(await searchPage.resultName()).to.equal(article);
+            addStep('Checks that the shopping cart did not change')
+            await $(".btn-inverse").waitForDisplayed();
+            addStep('compare images')
+            expect(
+                await browser.checkElement(await $(".btn-inverse"), "opencart-ShoppingCart", {
+                }),
+                "Error: Shopping Cart has changed").equal(0);
+        });
+    });
+});
